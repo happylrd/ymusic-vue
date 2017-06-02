66b55f7a5793730bfd036611afcd8fd9abdbcfb7
happylrd <happylrd17@gmail.com>
2017-06-02 15:34:41 +0800

    Add style and image

--- ymusic/src/App.vue
+++ ymusic/src/App.vue
@@ -10,5 +10,8 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable"
 
+  #app
+    color: $color-theme
 </style>
