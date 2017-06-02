8323276dd9bb39ec17886d47365dc2651595912b
happylrd <happylrd17@gmail.com>
2017-06-02 19:43:00 +0800

    Add jsonp util

---
+++ ymusic/src/common/js/jsonp.js
@@ -0,0 +1,27 @@
+import originalJSONP from 'jsonp'
+
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originalJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function param (data) {
+  let url = ''
+
+  // TODO: will add `hasOwnProperty` later
+  for (let key in data) {
+    let value = data[key] !== undefined ? data[key] : ''
+    url += `&${key}=${encodeURIComponent(value)}`
+  }
+
+  return url ? url.substring(1) : ''
+}
