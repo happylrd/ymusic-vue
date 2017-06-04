d79b294bd13a2c61b6368abcdd62cfc801933e91
happylrd <happylrd17@gmail.com>
2017-06-04 12:11:53 +0800

    Add lazy load for image in list
    and fix the conflict between fastclick and better-scroll

--- ymusic/src/components/recommend/Recommend.vue
+++ ymusic/src/components/recommend/Recommend.vue
@@ -6,7 +6,7 @@
           <Slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" @load="loadImage"/>
+                <img class="needsclick" :src="item.picUrl" @load="loadImage"/>
               </a>
             </div>
           </Slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl"/>
+                <img width="60" height="60" v-lazy="item.imgurl"/>
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
