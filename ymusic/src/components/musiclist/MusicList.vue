2893c6eaa6e2a41edffeaa860b3f6be4c356ae14
happylrd <happylrd17@gmail.com>
2017-07-29 22:27:39 +0800

    Improve ui

--- ymusic/src/components/musiclist/MusicList.vue
+++ ymusic/src/components/musiclist/MusicList.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
 
-    <div class="bg-layer" ref="layer">
-
-    </div>
+    <div class="bg-layer" ref="layer"></div>
 
     <Scroll @scroll="scroll"
             :probeType="probeType" :listenScroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
         <SongList :songs="songs"></SongList>
+      </div>
+
+      <div class="loading-container" v-show="!songs.length">
+        <Loading></Loading>
       </div>
     </Scroll>
   </div>
@@ -24,6 +32,7 @@
 <script>
   import Scroll from 'base/scroll/Scroll'
   import SongList from 'base/songlist/SongList'
+  import Loading from 'base/loading/Loading'
   import { prefixStyle } from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40
@@ -67,6 +76,9 @@
     methods: {
       scroll (pos) {
         this.scrollY = pos.y
+      },
+      back () {
+        this.$router.back()
       }
     },
     watch: {
@@ -88,9 +100,11 @@
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
         this.$refs.bgImage.style[transform] = `scale(${scale})`
@@ -98,7 +112,8 @@
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
